9d2d3b1db16e64d68ecdc2d2b78e58372d0e1835
Magamahe <magamahe@gmail.com>
2025-11-28 17:01:54 -0300

    API los Simpson

--- clase_13/API_Simpson/app.js
+++ clase_13/API_Simpson/app.js
@@ -27,17 +27,17 @@ fetch("https://thesimpsonsapi.com/api/characters")
         <div class="flex w-full items-center justify-between space-x-6 p-6">
           <div class="flex-1 truncate">
             <div class="flex items-center space-x-3">
-              <h3 class="truncate text-sm font-medium text-gray-900">${name}</h3>
-              <span class="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-blue-600 ring-1 ring-inset ring-green-600/20">
-                ${age ? age : "Desconocido"}
+              <h3 class="truncate text-lg font-medium text-gray-900">${name}</h3>
+              <span class="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-sm font-medium text-blue-600 ring-1 ring-inset ring-green-600/20">
+                ${age ? age : "s/d"}
               </span>
-              <span class="inline-flex flex-shrink-0 items-center rounded-full bg-gray-200 px-1.5 py-0.5 text-xs font-medium text-gray-700">
+              <span class="inline-flex flex-shrink-0 items-center rounded-full bg-gray-200 px-1.5 py-0.5 text-sm font-medium text-gray-700">
                 ${status ? status : "Desconocido"}
               </span>
             </div>
             <p class="mt-1 truncate text-sm text-gray-500">${occupation}</p>
           </div>
-          <img class="h-15 w-14 flex-shrink-0 bg-gray-300 object-contain" src="https://cdn.thesimpsonsapi.com/500${portrait_path}" alt="">
+          <img class="h-20 w-20 flex-shrink-0  rounded-full bg-gray-300 object-cover" src="https://cdn.thesimpsonsapi.com/500${portrait_path}" alt="">
         </div>
         <div class="px-6 py-4">
           <p class="text-sm text-gray-900 w-full truncate text-center italic" style="color: oklch(43.2% 0.232 292.759);">
